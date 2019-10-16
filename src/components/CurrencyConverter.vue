c01416da7e1d53f194a86362f16558664b81da8d
Dexter Gapuz <gapuzd9@gmail.com>
2019-10-16 09:23:38 +0800

    feat: currency converter (#3)
    * development
    
    * feat: currency converter

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,65 +1,118 @@
 <template>
-    <v-form v-model="valid">
-    <v-container>
+  <v-container>
+    <h2>Currency Converter</h2>
+    <v-form ref="currencyForm">
       <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
+        <v-col cols="2">
           <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
+            type="number"
+            v-model="amount"
+            label="Amount"
+            :rules="amountRules"
           ></v-text-field>
         </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
+        <v-col cols="5">
+          <v-select
+            v-model="currentCurrency"
+            label="Current Currency"
+            :items="currencyItems"
+            :rules="currentRules"
+          ></v-select>
         </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+        <v-col cols="5">
+          <v-select
+            v-model="convertCurrency"
+            label="Convert to"
+            :items="currencyItems"
+            multiple
+            :rules="convertRules"
+          ></v-select>
         </v-col>
       </v-row>
-    </v-container>
-  </v-form>
+      <v-row align="center" justify="center">
+        <v-btn depressed color="primary" @click="convert">Convert</v-btn>
+      </v-row>
+    </v-form>
+    <v-row>
+      <v-col cols="6" v-for="(item, index) in dataItem" :key="index">
+        <v-card>
+          <v-card-text>
+            <!-- {{ item.amount }}, {{ item.curr }} -->
+            {{ item.amount + " " + item.from }} = {{ item.computedAmount + " " + item.curr }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    data: () => ({
-      valid: false,
-      firstname: '',
-      lastname: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
-      ],
-    }),
+  data: () => ({
+    // valid:
+    amount: null,
+    convertedAmount: null,
+    currentCurrency: null,
+    convertCurrency: [],
+    currencyItems: [],
+    dataItem: [],
+    amountRules: [
+      v => !!v || 'This field is required'
+    ],
+    currentRules: [
+      v => !!v || 'This field is required'
+    ],
+    convertRules: [
+      v => v.length > 0 || 'This field is required',
+      v => v.length < 5 || 'Maximum of 5 selected items only'
+    ]
+  }),
+
+  created() {
+    let config = {
+      method: 'GET',
+      url: 'https://currency-exchange.p.rapidapi.com/listquotes',
+      headers: {
+        "x-rapidapi-host":"currency-exchange.p.rapidapi.com",
+        "x-rapidapi-key":"4sMe2fB80bmshubyDEw1XaQzamkRp15CeyrjsnjXnRhVm9rr1o"
+      }
+    };
+    axios(config)
+      .then(res => this.currencyItems = res.data)
+      .catch(err => console.log(err));
+  },
+
+  methods: {
+    convert() {
+      if(this.$refs.currencyForm.validate()) {
+        this.dataItem = [];
+        this.convertCurrency.forEach(item => {
+          let config = {
+            method: 'GET',
+            url: 'https://currency-exchange.p.rapidapi.com/exchange',
+            params: {
+              from: this.currentCurrency,
+              to: item
+            },
+            headers: {
+              "x-rapidapi-host":"currency-exchange.p.rapidapi.com",
+              "x-rapidapi-key":"4sMe2fB80bmshubyDEw1XaQzamkRp15CeyrjsnjXnRhVm9rr1o"
+            }
+          }
+          axios(config)
+            .then(res => {
+              // let computedAmount = this.amount * res.data;
+              this.dataItem.push({ computedAmount: (this.amount * res.data).toFixed(2), curr: item, from: this.currentCurrency, amount: this.amount});
+              
+            })
+            .catch(err => console.log(err));
+        });
+        console.log(this.dataItem);
+      }
+    }
+  }
 }
 </script>
 
